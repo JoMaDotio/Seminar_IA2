82f390685926fdcf0c7429371fd33db64e323d98
JoMaDotio <eltaquero@hotmail.com>
2022-09-26 08:05:16 -0500

    its all for today

---
+++ classes/class_12/main.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.manifold import TSNE
+
+data = pd.read_csv('../../data/mnist_784.csv')
+n_samples = 3000
+
+x = np.asanyarray(data.drop(columns=['class']))[:n_samples]
+y = np.asanyarray(data[['class']])[:n_samples]
+
+model = TSNE(n_components=2, n_iter=2000)
+
+x_2d = model.fit_transform(x)
+
+plt.scatter(x_2d[:, 0], x_2d[:,1], c=y, cmap=plt.cm.tab10)
